068a3dd9319500b40534753ec5c13b17b2a80167
namanjhalani <140639507+namanjhalani@users.noreply.github.com>
2026-02-13 19:00:48 +0530

    Add files via upload
    Initial project upload

---
+++ career_data.py
@@ -0,0 +1,59 @@
+career_data = {
+
+    "pcm": [
+        ("Engineering (IIT / NIT / Top State Colleges)", "computer"),
+        ("Architecture (B.Arch)", "creative"),
+        ("B.Tech / BE (Private / State)", "computer"),
+        ("Defence (NDA / TES)", "discipline"),
+        ("B.Sc Maths / Physics / CS", "research"),
+        ("BCA → MCA", "computer"),
+        ("Polytechnic (Diploma)", "practical"),
+        ("B.Sc Computer Science / IT", "computer")
+    ],
+
+    "pcb": [
+        ("MBBS (Doctor)", "medical"),
+        ("BDS (Dental Surgeon)", "medical"),
+        ("BAMS / BHMS (AYUSH)", "medical"),
+        ("B.Pharmacy", "medical"),
+        ("B.Sc Biotechnology / Microbiology", "research"),
+        ("B.Sc Nursing", "medical"),
+        ("BPT (Physiotherapy)", "medical"),
+        ("Veterinary Science", "medical"),
+        ("BMLT", "medical"),
+        ("Nutrition & Public Health", "medical")
+    ],
+
+    "pcmb": [
+        ("Engineering", "computer"),
+        ("MBBS", "medical"),
+        ("Biotechnology", "research"),
+        ("BCA → MCA", "computer")
+    ],
+
+    "commerce": [
+        ("Chartered Accountant (CA)", "finance"),
+        ("Company Secretary (CS)", "management"),
+        ("CMA", "finance"),
+        ("B.Com", "business"),
+        ("BBA", "business"),
+        ("Economics", "analysis"),
+        ("Banking Exams", "finance"),
+        ("BCA", "computer"),
+        ("Hotel Management", "management"),
+        ("Digital Marketing / Analytics", "creative")
+    ],
+
+    "arts": [
+        ("Civil Services", "leadership"),
+        ("Law", "analysis"),
+        ("Bachelor of Arts (BA)", "general"),
+        ("Journalism & Mass Communication", "creative"),
+        ("Psychology", "helping"),
+        ("Bachelor of Education (B.Ed)", "teaching"),
+        ("Hotel Management", "management"),
+        ("Social Work (BSW / MSW)", "helping"),
+        ("Design (Fashion / Graphic / Interior)", "creative"),
+        ("Content Writing / Digital Marketing", "creative")
+    ]
+}
